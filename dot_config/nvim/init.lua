a9821dae055e7441fbc6f91b396f2f21cb246eb6
Joseph <hejops1@gmail.com>
2024-12-14 14:16:56 +0100

    move vim.filetype.add out to filetypes.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,6 +5,7 @@ require("util")
 
 require("autocmds")
 require("binds")
+require("filetypes")
 require("sets")
 
 require("plugins")
@@ -1227,29 +1228,3 @@ require("cmp-gitcommit").setup({}) -- i don't really use this
 
 require("util"):random_colorscheme()
 vim.keymap.set("n", "<F12>", require("util").random_colorscheme)
-
--- is there a better place to put this?
-vim.filetype.add({
-	pattern = {
-		-- https://github.com/emilioziniades/dotfiles/blob/db7b414c150d3a3ab863a0109786f7f48465dd23/nvim/init.lua#L708
-		[".*/templates/.*.html"] = function(_, bufnr)
-			local content = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-			for _, line in ipairs(content) do
-				if line:match("{%%") or line:match("%%}") then
-					return "htmldjango"
-				end
-			end
-		end,
-		["Dockerfile.*"] = "dockerfile",
-		[".+%.flux"] = "flux",
-		["docker%-compose.*.yml"] = "yaml.docker-compose", -- '-' has special meaning (smh)
-	},
-
-	-- https://github.com/kennethnym/dotfiles/blob/41f03b9091181dc62ce872288685b27f001286f3/nvim/init.lua#L474
-	filename = {
-
-		["Dockerfile"] = "dockerfile",
-		["docker-compose.yml"] = "yaml.docker-compose",
-		["yarn.lock"] = "text", -- default is yaml for some reason
-	},
-})
