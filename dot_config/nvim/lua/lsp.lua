41df65bd5d07bd8db04d9dfedb9087f2966fa369
Joseph <hejops1@gmail.com>
2025-01-01 21:19:16 +0100

    remove c build flags from clangd

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -262,16 +262,14 @@ local servers = { -- {{{
 
 				"-I.", -- https://gcc.gnu.org/onlinedocs/gcc/Directory-Options.html#index-I
 
+				-- "Since -Weverything enables every diagnostic, we generally don’t recommend using it."
+
 				"-Wall", -- https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wall
 				"-Wextra", -- https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-W
 				"-Wpedantic", -- https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-pedantic-1
 
 				"-Wshadow",
 				"-Wconversion",
-
-				-- https://clang.llvm.org/docs/ClangCommandLineReference.html#target-independent-compilation-options
-				"-ferror-limit=0",
-				"-ftrivial-auto-var-init=zero",
 			},
 
 			-- https://github.com/kuprTheMan/dotfiles/blob/b340744e0b4964e02ea43f34d4fd8303e6e8c644/.config/nvim/init.lua#L670
