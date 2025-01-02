8e65fb9b9b055c88c2b12e6a4646f57e6b970697
Joseph <hejops1@gmail.com>
2025-01-02 14:36:27 +0100

    add yaml.github, dotenv

--- dot_config/nvim/lua/filetypes.lua
+++ dot_config/nvim/lua/filetypes.lua
@@ -1,14 +1,18 @@
+-- https://github.com/ahmedelgabri/dotfiles/blob/main/config/nvim/filetype.lua
+
 vim.filetype.add({
 	pattern = {
+
+		-- [".+%.flux"] = "flux",
+		[".*/%.github/.*%.ya?ml"] = "yaml.github",
 		[".*/templates/.*.html"] = require("util"):buf_contains("{%%") and "htmldjango" or "html", -- https://github.com/emilioziniades/dotfiles/blob/db7b414c/nvim/init.lua#L714
 		["Dockerfile.*"] = "dockerfile",
-		[".+%.flux"] = "flux",
-		["docker%-compose.*.yml"] = "yaml.docker-compose", -- '-' has special meaning (smh)
 	},
 
 	-- https://github.com/kennethnym/dotfiles/blob/41f03b9091181dc62ce872288685b27f001286f3/nvim/init.lua#L474
 	filename = {
 
+		[".env"] = "dotenv",
 		["Dockerfile"] = "dockerfile",
 		["docker-compose.yml"] = "yaml.docker-compose",
 		["yarn.lock"] = "text", -- default is yaml for some reason
