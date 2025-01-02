acd3aae679687f342fa1d89f0f26c8a00dbd2c48
Joseph <hejops1@gmail.com>
2025-01-02 14:24:43 +0100

    remove telescope-fzf-native

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -352,13 +352,6 @@ require("lazy").setup(
 			},
 		},
 
-		-- {
-		-- 	"nvim-telescope/telescope-fzf-native.nvim",
-		-- 	build = "make",
-		-- 	cond = function()
-		-- 		return vim.fn.executable("make") == 1
-		-- 	end,
-		-- },
 		-- }}}
 		{ -- trouble {{{
 			"folke/trouble.nvim",
