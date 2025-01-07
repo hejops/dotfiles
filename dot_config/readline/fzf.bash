1bbfd14b72b7786a0a24c6b38ff80025c12394d3
Joseph <hejops1@gmail.com>
2025-01-07 13:31:17 +0100

    rewrite single line if/else

--- dot_config/readline/fzf.bash
+++ dot_config/readline/fzf.bash
@@ -17,8 +17,14 @@
 # }
 
 __fzfcmd() {
-	[[ -n ${TMUX_PANE-} ]] && { [[ ${FZF_TMUX:-0} != 0 ]] || [[ -n ${FZF_TMUX_OPTS-} ]]; } &&
-		echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+	if
+		[[ -n ${TMUX_PANE-} ]] &&
+			{ [[ ${FZF_TMUX:-0} != 0 ]] || [[ -n ${FZF_TMUX_OPTS-} ]]; }
+	then
+		echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- "
+	else
+		echo "fzf"
+	fi
 }
 
 # fzf_file_widget() {
