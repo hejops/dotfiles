b695eec1b1b736f586a1453b567bf57ab360d34b
Joseph <hejops1@gmail.com>
2025-01-07 13:58:15 +0100

    use biome.json if it exists

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -778,13 +778,14 @@ require("conform").setup({
 			-- important: at work, use top-level biome.json
 			-- note: cwd must be a func, not a string
 			cwd = require("util").root_directory,
-			-- TODO: if biome.json exists, leave args unchanged
-			args = {
-				"format",
-				"--indent-style=space",
-				"--stdin-file-path",
-				"$FILENAME",
-			},
+			args = vim.loop.fs_stat(require("util"):root_directory() .. "/biome.json") -- if biome.json exists, leave args unchanged
+					and { "format", "--stdin-file-path", "$FILENAME" }
+				or {
+					"format",
+					"--indent-style=space",
+					"--stdin-file-path",
+					"$FILENAME",
+				},
 		},
 
 		shfmt = {
