e4a664d671a96aff1e684b6abeaf7222555d978f
Joseph <hejops1@gmail.com>
2025-01-07 13:53:01 +0100

    tidy font config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -142,9 +142,7 @@ config.switch_to_last_active_tab_when_closing_tab = true
 
 -- wezterm ls-fonts --list-system | cut -d, -f1 | grep ^wez | cut -d'(' -f2 | sort -u | append ,
 
--- note: source han sans is implicitly used as fallback for cn/jp/kr (which is
--- why scp goes great with it). scp also has cyrillic
-local font = wezterm.font_with_fallback({
+local fonts = {
 
 	-- https://devfonts.gafi.dev
 	-- https://www.codingfont.com
@@ -166,7 +164,7 @@ local font = wezterm.font_with_fallback({
 	-- "Geist Mono", -- chonky
 
 	-- narrow
-	-- "NanumGothicCoding", -- only has underline >= 12
+	-- "NanumGothicCoding", -- only has underline at certain sizes/resolutions (<= 13/2k)
 	-- "Mplus Code 60", -- too tall, otherwise quite good
 	-- "Iosevka",
 
@@ -176,7 +174,7 @@ local font = wezterm.font_with_fallback({
 	-- "SF Mono", -- '--' too close (almost ligature-ish)
 	-- "Input Mono", -- why so short?
 	"Source Code Pro",
-})
+}
 
 -- TODO: and not dwm
 -- if is_ubuntu then
@@ -187,39 +185,45 @@ local font = wezterm.font_with_fallback({
 -- 	end)
 -- end
 
--- config.freetype_load_flags = "NO_HINTING" -- squashes fonts (makes them shorter)
-
--- -- yikes
 -- config.freetype_load_flags = "MONOCHROME"
--- config.freetype_load_target = "Mono"
-
-local cell_width = 0.9
+-- config.freetype_load_flags = "NO_HINTING" -- squashes fonts (makes them shorter)
+-- config.freetype_load_target = "Mono" -- yikes
 
 -- note: entire config is evaluated on window open (but not close), tab
 -- open/close, but not on split
 
 -- relying on xrdb dpi is unreliable, as ubuntu seems to ignore it
+local cell_width = 0.9
 local font_size
-if is_ubuntu then
-	-- note: xrandr is unacceptably slow
-	font_size = get_output("xdpyinfo | grep -F 'x1200'") and 12.0 -- (dual) 2k
-		or get_output("xdpyinfo | grep -F '1920x1080'") and 16.0 -- laptop-only (2k)
-		or 18.0 -- home, 4k only
+
+if not is_ubuntu then
+	-- home, 4k only
+	font_size = 18.0
+elseif get_output("xdpyinfo | grep -F 'x1200'") then -- note: xrandr is unacceptably slow
+	-- (dual) 2k
+	table.insert(fonts, 1, "NanumGothicCoding")
+	font_size = 12.0
+elseif get_output("xdpyinfo | grep -F '1920x1080'") then
+	-- laptop-only (2k)
+	font_size = 16.0
 else
-	font_size = 10.0 -- home, 4k
+	-- home, 4k
+	font_size = 10.0
 end
 
--- font_size = 12.0 -- home, 4k
+-- note: source han sans is implicitly used as fallback for cn/jp/kr (which is
+-- why scp goes great with it). scp also has cyrillic
+local main_font = wezterm.font_with_fallback(fonts)
 
 -- config.font, config.font_size = wezterm.font_with_fallback({ "B612 Mono" }), 9.0
 -- config.font, config.font_size = wezterm.font_with_fallback({ "Source Code Pro" }), 10.0
 config.cell_width = cell_width > 0 and cell_width or 0.9 -- negative values will kill wezterm
 config.command_palette_font_size = font_size
-config.font = font
+config.font = main_font
 config.font_size = font_size
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.warn_about_missing_glyphs = false
-config.window_frame = { font = font, font_size = font_size }
+config.window_frame = { font = main_font, font_size = font_size }
 
 -- }}}
 -- appearance {{{
