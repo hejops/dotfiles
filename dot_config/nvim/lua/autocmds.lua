7afdd68c1d998a6d4404792d67aae74de9e5f74d
Joseph <hejops1@gmail.com>
2025-01-08 12:11:52 +0100

    fix: disable automatic code_action in ts

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -140,7 +140,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		end
 
 		if not vim.loop.fs_stat(".markuplintrc.json") then
-			os.execute([[	
+			os.execute([[
 				echo '{
 					"extends": ["markuplint:recommended"],
 					"parser": {"\\.html$": "@markuplint/htmx-parser"},
@@ -222,23 +222,22 @@ vim.api.nvim_create_autocmd("TermOpen", {
 -- 	end,
 -- })
 
--- https://github.com/Virus288/Neovim-Config/blob/5cb7f321d217d3f9163ddc26c4b25c4c35f80211/lua/configs/lspConfig.lua#L95
--- auto import missing files on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	group = vim.api.nvim_create_augroup("TS_add_missing_imports", { clear = true }),
-	desc = "TS_add_missing_imports",
-	pattern = { "*.ts" },
-	callback = function()
-		vim.lsp.buf.code_action({
-			apply = true,
-			context = {
-				diagnostics = {},
-				only = { "refactor" },
-			},
-		})
-		vim.cmd("write")
-	end,
-})
+-- https://github.com/Virus288/Neovim-Config/blob/5cb7f321/lua/configs/lspConfig.lua#L95
+-- WARN: this applies -all- code actions, even ones that are undesired (e.g.
+-- export default), so this is almost always a bad idea
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = { "*.ts" },
+-- 	callback = function()
+-- 		vim.lsp.buf.code_action({
+-- 			apply = true,
+-- 			context = {
+-- 				diagnostics = {},
+-- 				only = { "refactor" },
+-- 			},
+-- 		})
+-- 		vim.cmd("write")
+-- 	end,
+-- })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c" },
