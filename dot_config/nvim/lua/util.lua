a009b6ff194e807076139510dbd42d15a50f7eea
Joseph <hejops1@gmail.com>
2025-04-09 21:09:24 +0200

    simplify root_directory

--- dot_config/nvim/lua/util.lua
+++ dot_config/nvim/lua/util.lua
@@ -230,13 +230,14 @@ end
 -- attempt to get git repo root, ignoring all possible child directories
 ---@return string|nil
 function M:root_directory()
-	local cmd = "git -C " .. vim.fn.shellescape(vim.fn.expand("%:p:h")) .. " rev-parse --show-toplevel"
-	local toplevel = vim.fn.system(cmd)
-	if not toplevel or #toplevel == 0 or toplevel:match("fatal") then
-		-- return vim.fn.getcwd()
-		return nil
-	end
-	return toplevel:sub(0, -2)
+	return vim.fs.root(0, ".git")
+	-- local cmd = "git -C " .. vim.fn.shellescape(vim.fn.expand("%:p:h")) .. " rev-parse --show-toplevel"
+	-- local toplevel = vim.fn.system(cmd)
+	-- if not toplevel or #toplevel == 0 or toplevel:match("fatal") then
+	-- 	-- return vim.fn.getcwd()
+	-- 	return nil
+	-- end
+	-- return toplevel:sub(0, -2)
 end
 
 -- }}}
