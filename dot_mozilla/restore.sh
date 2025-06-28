d12f01ddbe8f7c17b1513d734d87c3e445c7f3d2
Joseph <hejops1@gmail.com>
2025-06-28 16:51:10 +0200

    cleanup

--- dot_mozilla/restore.sh
+++ dot_mozilla/restore.sh
@@ -8,11 +8,23 @@ else
 	MOZ_DIR=~/.mozilla
 fi
 
-rm -rf "$MOZ_DIR"/firefox
+# find "$MOZ_DIR" -name recovery.jsonlz4
+# mozlz4 -x ~/.mozilla/firefox/*default/sessionstore-backups/recovery.jsonlz4 |
+# 	jq -r '.windows[0].tabs[].entries[-1].url' |
+# 	grep ^http |
+# 	sort -u
+
+# note: logins.json can never be restored
+
+# rm -rf "$MOZ_DIR" # would remove restore.sh
+
 rm -rf "$MOZ_DIR"/extensions
+rm -rf "$MOZ_DIR"/firefox
 rm -rf "$MOZ_DIR"/native-messaging-hosts
 
 mkdir -p "$MOZ_DIR"/firefox
+
+# TODO: random hex? can it be 1 char?
 
 cat << EOF > $MOZ_DIR/firefox/profiles.ini
 [Install4F96D1932A9F858E]
@@ -37,12 +49,13 @@ cp -r ~/.local/share/chezmoi/dot_mozilla/firefox/4clnophl.default "$MOZ_DIR"/fir
 # https://github.com/LukeSmithxyz/LARBS/blob/master/static/larbs.sh#L232
 # https://stackoverflow.com/a/37739112
 
-EXT_DIR=$MOZ_DIR/firefox/default/extensions
+NEW_PROFILE_DIR=$MOZ_DIR/firefox/default
+EXT_DIR=$NEW_PROFILE_DIR/extensions
 mkdir -p "$EXT_DIR"
 
 addons=(
 
-	# auto-tab-discard # native of ff 93 https://support.mozilla.org/en-US/kb/unload-inactive-tabs-save-system-memory-firefox
+	# auto-tab-discard # native as of ff 93 https://support.mozilla.org/en-US/kb/unload-inactive-tabs-save-system-memory-firefox
 	cookie-autodelete
 	tridactyl-vim
 	ublock-origin
@@ -79,8 +92,6 @@ for addon in "${addons[@]}"; do
 	mv "$file" "$EXT_DIR/$id.xpi"
 done
 
-# TODO: sidebar = tst (workaround: xdotool (which is already in tri))
-
 rm -rf "$tmpdir"
 
 if [ ! -f "$MOZ_DIR"/native-messaging-hosts/tridactyl.json ]; then
@@ -100,12 +111,6 @@ fi
 
 # ublock: google -- disable inline scripts
 
-# # something in my userchrome prevents addon popup from being clicked,
-# # apparently
-# sed -i -r '/legacyUserProfileCustomizations/ s#^#//#' $MOZ_DIR/firefox/default/user.js
-# firefox 'about:addons' # manually enable addons
-# sed -i -r '/legacyUserProfileCustomizations/ s#^// *##' $MOZ_DIR/firefox/default/user.js
-
 # https://github.com/BryceVandegrift/ffsetup/blob/4b17486ed6e1360076f3f8f297d9cde7adad5c9c/ffsetup.sh#L67
 firefox --headless > /dev/null 2>&1 & # generate extensions.json
 sleep 10
@@ -116,34 +121,34 @@ sed -i '
 ' "$MOZ_DIR"/firefox/default/extensions.json
 sed -i 's/\(extensions\.pendingOperations", \)false/\1true/' "$MOZ_DIR"/firefox/default/prefs.js
 
-# # TODO: cookies.sqlite -- block cookies on consent.youtube.com
-# sqlite3 $FF_PROFILE_DIR/cookies.sqlite "INSERT INTO moz_cookies VALUES(5593,'^firstPartyDomain=youtube.com','CONSENT','PENDING+447','.youtube.com','/',1723450203,1660378445948074,1660378204032779,1,0,0,1,0,2);"
-# INSERT INTO moz_cookies VALUES(2358,'^firstPartyDomain=youtube.com','CONSENT','PENDING+675','.youtube.com','/',1727208372,1664136373196881,1664136373196881,1,0,0,1,0,2);
-
 # pre-installed search engines can only be hidden, not removed (this is why the
 # default engines can -always- be restored)
 search_lz4=$MOZ_DIR/firefox/default/search.json.mozlz4
-wget https://github.com/jusw85/mozlz4/releases/download/v0.1.0/mozlz4-linux
-chmod +x mozlz4-linux
+
 # disable all engines except ddg
-./mozlz4-linux -x "$search_lz4" |
+# might not work? (reverts to google)
+mozlz4 -x "$search_lz4" |
 	jq '.engines[] | ._metaData.hidden = .id != "ddg"' |
-	./mozlz4-linux -z - "$search_lz4"
-rm mozlz4-linux
+	mozlz4 -z - "$search_lz4"
 
 # need gui startup to create xulstore
 firefox
 
 # autohide menu bar
-< ~/.mozilla/firefox/default/xulstore.json jq '."chrome://browser/content/browser.xhtml"."toolbar-menubar".autohide = true'
+< "$MOZ_DIR"/firefox/default/xulstore.json jq '."chrome://browser/content/browser.xhtml"."toolbar-menubar".autohide = true'
 
 # activate TST sidebar
 if [[ -n $need_tst ]]; then
-	< ~/.mozilla/firefox/default/xulstore.json jq '."chrome://browser/content/browser.xhtml"."sidebar-title".value = "Tree Style Tab"'
+	# TODO: sidebar = tst (workaround: xdotool (which is already in tri))
+	< "$MOZ_DIR"/firefox/default/xulstore.json jq '."chrome://browser/content/browser.xhtml"."sidebar-title".value = "Tree Style Tab"'
 fi
 
 sqlite3 "$MOZ_DIR"/firefox/default/places.sqlite "DELETE FROM moz_bookmarks;"
 sqlite3 "$MOZ_DIR"/firefox/default/places.sqlite "DELETE FROM moz_places;"
 
 # manual action required (why?):
-# customize toolbar (remove spaces, add search bar)
+# disable, then enable addons (about:addons)
+# restore tabs (make sure tri is enabled first, else not grouped)
+# remove elements from toolbar (warn: removing 'list all tabs' also removes sidebar?)
+# set search engine
+# restore ublock
