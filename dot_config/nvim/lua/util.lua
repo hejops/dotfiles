3856bf27713b980fd260c05613feb1bf746ee8bf
Joseph <hejops1@gmail.com>
2025-09-20 09:30:39 +0200

    simplify random_colorscheme

--- dot_config/nvim/lua/util.lua
+++ dot_config/nvim/lua/util.lua
@@ -205,57 +205,11 @@ function M:random_colorscheme() -- {{{
 	-- in case light theme is needed (sunset), zellner is decent
 	-- https://tilde.club/~woland/vim-colorschemes/
 
-	-- sliced and diced from tssm/nvim-random-colors
+	-- ~/.local/share/nvim/lazy/*/colors/*.{lua,vim}
 
-	-- ~/.local/share/nvim/lazy/*/colors/*.(lua|vim)
-
-	local function map(f, xs)
-		local result = {}
-		for _, x in ipairs(xs) do
-			local mapped = f(x)
-			local function _0_()
-				if 0 == select("#", mapped) then
-					return nil
-				else
-					return mapped
-				end
-			end
-			table.insert(result, _0_())
-		end
-		return result
-	end
-
-	local function concat(...)
-		local function map2(f, xs)
-			for _, x in ipairs(xs) do
-				f(x)
-			end
-		end
-		local result = {}
-		local function append(xs)
-			for _, x in ipairs(xs) do
-				table.insert(result, x)
-			end
-		end
-		map2(append, { ... })
-		return result
-	end
-
-	local root = "~/.local/share/nvim/lazy/" -- globpath expands ~, apparently?
-	local glob = "*/colors/*.%s"
-
-	local paths = concat(
-		vim.fn.globpath(root, string.format(glob, "lua"), false, true),
-		vim.fn.globpath(root, string.format(glob, "vim"), false, true)
-	)
-
-	local function scheme_name(path)
-		return vim.fn.fnamemodify(path, ":t:r") -- :t tail (basename), :r root (strip extension)
-	end
-
-	local all_schemes = map(scheme_name, paths)
-	local scheme = all_schemes[math.random(#all_schemes)]
-
+	local files = vim.fn.globpath("~/.local/share/nvim/lazy/", "*/colors/*.{lua,vim}", false, true)
+	local f = files[math.random(#files)]
+	local scheme = vim.fn.fnamemodify(f, ":t:r")
 	vim.cmd.colorscheme(scheme)
 end -- }}}
 
