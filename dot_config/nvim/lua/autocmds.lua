cd5860892a4ecbb57571c9b9752c545ce8971d1c
Joseph <hejops1@gmail.com>
2026-06-18 09:14:13 +0200

    skip mkdir if parent has $

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -111,6 +111,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = vim.fn.expand("~") .. "/.ssh/config",
 	callback = function()
+		-- TODO: move to conform
 		os.execute([[sed -r -i 's/^[ \t]*/\t/g; s/^\t(Host .+)?$/\1/' ]] .. vim.fn.expand("%"))
 		vim.cmd("e")
 	end,
@@ -181,6 +182,10 @@ vim.api.nvim_create_autocmd("BufNewFile", {
 	callback = function()
 		local parent = vim.fn.expand("%:p:h")
 		if vim.uv.fs_stat(parent) then
+			return
+		end
+
+		if parent:match("%$") then
 			return
 		end
 
